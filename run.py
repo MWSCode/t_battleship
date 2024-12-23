25a1c0d415c39903067a37ef4642955326fc0409
MWResearch <mrbehfarshad-mail@yahoo.com>
2024-12-23 21:21:38 +0000

    create necessary  variables and functions

--- run.py
+++ run.py
@@ -1,3 +1,45 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+"""
+TERMINAL BATTLESHIP v1
+
+help resource links:
+
+
+"""
+
+from random import randint
+
+# variables:
+ships = 4       # number of ships
+size = [4, 5]   # board-size: row, column
+player_hit = 0
+computer_hit = 0
+
+# print the board
+def print_board(board):
+    pass
+
+# create a board
+def create_board():
+    pass
+
+# fill the board with ships:
+def place_ships(board):
+    pass
+
+# get player inputs
+def get_player_input():
+    pass
+
+def check_hit(board, row, col):
+    pass
+
+def computer_hit_func(board):
+    pass
+
+def game_start_end(u_input):
+    print("test")
+
+def game_play():
+    pass
+
+game_start_end("")
